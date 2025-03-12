e66ea996a9a38dc7453ebe9096eaa3cf2e50a735
pushp2401 <pushp.kumari@doaz.ai>
2025-03-12 12:26:08 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -32,9 +32,9 @@ st.title("Mass cold email sender")
 uploaded_file = st.file_uploader( "Upload you client list as csv here" ,type=["csv"])
 if uploaded_file is not None : 
     subject_input = st.text_area("Enter mail subject:" , placeholder = "Followup regarding data science budget")
-    body_input = st.text_area("Enter mail body:" , placeholder = "This mail is regarding...Best regards,\n Barbara Daniels")
+    body_input = st.text_area("Enter mail body:" , placeholder = "This mail is regarding...\nBest regards,\n Barbara Daniels")
     uploaded_pictures = st.file_uploader("Optional : Upload Images to attach : ", type=["png", "jpg", "jpeg"] , accept_multiple_files = True)
-    signature_input = st.text_area("Enter email signature : " , placeholder = "-------------\n Barbara daniels\n VP, XYZ Ltd\n +91-1234567890\nAtlanta, USA ")
+    signature_input = st.text_area("Enter email signature : " , placeholder = "\n Barbara daniels\n VP, XYZ Ltd\n +91-1234567890\nAtlanta, USA ")
     if st.button("Send mail") :
         if subject_input and body_input:
             if uploaded_file :
@@ -43,7 +43,7 @@ if uploaded_file is not None :
                     df = pd.read_csv(uploaded_file)
                     for one_mail in df['mail'] :
                         name = extract_name(one_mail)
-                        full_mail_content = "Hello " + name + ",\n" + body_input
+                        full_mail_content = "Hello " + name + ",\n" + body_input + "\n" + "---------------" +  "\n\n"
                         if signature_input is not None : 
                             signature_bold = f"""
                             <html>
